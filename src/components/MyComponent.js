f167d614c8c1fea7e347779a23ae2a0c70274f25
Nguyen Tuan Phong <tuanphong5801@gmail.com>
2022-11-22 22:14:26 +0700

    section 22

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,51 +1,12 @@
 import React from "react";
+import UserInfo from "./UserInfo";
 
 class MyComponent extends React.Component {
-  state = {
-    name: "Tuan Phong",
-    address: "hanoi",
-    age: 21,
-  };
-
-  handleClick = (event) => {
-    // console.log("Click my button!");
-    console.log(`My name is: ${this.state.name}`);
-    console.log(`I'm: ${this.state.age}`);
-
-    this.setState({
-      name: "Moc Ly",
-      age: Math.floor(Math.random() * 100 + 1),
-    });
-  };
-
-  handleOnChangeInput = (event) => {
-    this.setState({
-      name: event.target.value.trim(),
-    });
-  };
-
-  handleOnSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state);
-  };
   //JSX
   render() {
     return (
       <div>
-        My name is {this.state.name} and I'm {this.state.age}
-        <form
-          onSubmit={(event) => {
-            this.handleOnSubmit(event);
-          }}
-        >
-          <input
-            type="text"
-            onChange={(event) => {
-              this.handleOnChangeInput(event);
-            }}
-          />
-          <button>Submit</button>
-        </form>
+        <UserInfo />
       </div>
     );
   }
