bbe026f063c448724170fad481ed4d11c8149bf0
Nguyen Tuan Phong <tuanphong5801@gmail.com>
2022-11-22 10:33:04 +0700

    section 18

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,11 +7,21 @@ class MyComponent extends React.Component {
     age: 21,
   };
 
+  handleClick(event) {
+    // console.log("Click my button!");
+    console.log(`My name is: `);
+  }
+
+  handleOnMouseOver(event) {
+    console.log(`x = ${event.clientX}, y = ${event.clientY}`);
+  }
   //JSX
   render() {
     return (
       <div>
         My name is {this.state.name} and I'm from {this.state.address}
+        <button onMouseOver={this.handleOnMouseOver}>Hover me!</button>
+        <button onClick={this.handleClick}>Click me!</button>
       </div>
     );
   }
