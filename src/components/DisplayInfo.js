47ebdbd03b220072cef2b69e3a1f488c5382843a
Nguyen Tuan Phong <tuanphong5801@gmail.com>
2022-11-23 06:24:31 +0700

    section 27

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./DisplayInfo.scss";
 
 class DisplayInfo extends React.Component {
   state = {
@@ -16,7 +17,7 @@ class DisplayInfo extends React.Component {
     //const listUser = this.props.listUser;
     //props => viết tắt của properties
     return (
-      <div>
+      <div className="display-info-container">
         <div>
           <span
             onClick={() => {
@@ -33,7 +34,9 @@ class DisplayInfo extends React.Component {
             {listUser.map((user, index) => {
               return (
                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                  <div>My name is {user.name}</div>
+                  <div style={{ color: "yellow", paddingTop: "20px" }}>
+                    My name is {user.name}
+                  </div>
                   <div>I'm {user.age}</div>
                   <hr />
                 </div>
