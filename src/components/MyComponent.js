f5f16f1fb4f09a6e3092449ce376b6a82f08ecbd
Nguyen Tuan Phong <tuanphong5801@gmail.com>
2022-11-22 10:03:44 +0700

    section 17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "Tuan Phong",
+    address: "hanoi",
+    age: 21,
+  };
+
   //JSX
   render() {
     return (
       <div>
-        My first component
-        {Math.random()}
+        My name is {this.state.name} and I'm from {this.state.address}
       </div>
     );
   }
