671589969f524ec2cfc77de6d445b82a04884841
Nguyen Tuan Phong <tuanphong5801@gmail.com>
2022-11-28 11:13:50 +0700

    section 52

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,4 +1,11 @@
+import videoHomepage from "../../assets/video-homepage.mp4";
 const HomePage = (props) => {
-  return <div className="homepage-container">home page home page</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay muted loop>
+        <source src={videoHomepage} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 export default HomePage;
