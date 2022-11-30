0219dc3b49233b24b057c6bdcb9305b9e098acd4
Nguyen Tuan Phong <tuanphong5801@gmail.com>
2022-11-30 23:43:02 +0700

    section 59

--- src/components/Admin/Content/ModalCreateUsers.js
+++ src/components/Admin/Content/ModalCreateUsers.js
@@ -3,6 +3,7 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { FcPlus } from "react-icons/fc";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const ModalCreateUsers = (props) => {
   const { show, setShow } = props;
@@ -32,19 +33,29 @@ const ModalCreateUsers = (props) => {
     }
   };
 
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
   const handleSubmitCreateUser = async () => {
     //validate
+    const isValidEmail = validateEmail(email);
 
-    //call apis
-    // let data = {
-    //   email: email,
-    //   password: password,
-    //   username: username,
-    //   role: role,
-    //   userImage: image,
-    // };
-    // console.log(data);
+    if (!isValidEmail) {
+      toast.error("Invalid email!");
+      return;
+    }
 
+    if (!password) {
+      toast.error("Invalid password!");
+      return;
+    }
+
+    //submit data
     const data = new FormData();
     data.append("email", email);
     data.append("password", password);
@@ -56,7 +67,15 @@ const ModalCreateUsers = (props) => {
       "http://localhost:8081/api/v1/participant",
       data
     );
-    console.log(">>> check res: ", res);
+    // console.log(">>> check res: ", res.data);
+    if (res.data && res.data.EC === 0) {
+      toast.success(res.data.EM);
+      handleClose();
+    }
+
+    if (res.data && res.data.EC !== 0) {
+      toast.error(res.data.EM);
+    }
   };
 
   return (
